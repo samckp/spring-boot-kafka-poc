3227c474d29d7a429b963a38623b4782cdbc2d77
samckp <sam.mca.liet@gmail.com>
2023-03-20 09:10:38 +0530

    updated

--- src/main/java/com/kafka/poc/config/KafkaAdminUtil.java
+++ src/main/java/com/kafka/poc/config/KafkaAdminUtil.java
@@ -15,9 +15,9 @@ public class KafkaAdminUtil {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("security.protocol", "PLAINTEXT");
-        props.put("sasl.mechanism", "PLAINTEXT");
-        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='myuser' password='mypassword';");
-
+//        props.put("sasl.mechanism", "PLAINTEXT");
+//        props.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username='myuser' password='mypassword';");
+//
         properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
         AdminClient adminClient = AdminClient.create(props);
